4b4aebe6b7e7b571e871295c666cb791c4950357
larakoseff <larakoseff@gmail.com>
2023-08-15 11:42:05 +0200

    Add more ref code

--- reference/conditional.js
+++ reference/conditional.js
@@ -18,3 +18,52 @@ console.log(result1 === 'not true')
 const result2 = (1===1 || 4===5)
 console.log(result2 === true ? 'a' : 'b')
 
+const first = '1'
+const second = '2'
+
+console.log(`${first} ${second}`)
+
+const active = false
+let user
+
+
+if (active !== true) {
+    user = null
+}
+
+const month = 'apr'
+
+switch (month) {
+    case 'dec': case 'jan': case 'feb': console.log('Summer'); break;
+    case 'mar': case 'apr': case 'may': console.log('Autumn'); break;
+    case 'jun': case 'jul': case 'aug': console.log('Winter'); break;
+    case 'sept':  case 'oct': case 'nov': console.log('Spring'); break; 
+    default: throw new Error ('Invalid month')
+}
+
+if (month === 'mar' || month === 'apr' || month === 'may' ) {
+    console.log ('Autumn')
+} 
+if (month === 'jun' || month === 'jul' || month === 'aug' ) {
+    console.log('winter')
+} 
+ if (month === 'sept' || month === 'oct' || month === 'nov') {
+    console.log('Spring')
+} 
+ if (month === 'dec' || month === 'jan' || month === 'feb') {
+    console.log('Summer')
+ }
+
+// } else {
+//     throw new Error ('Invalid month')
+// }
+
+
+const firstName = 'Schalk'
+const age = 10
+
+const isValid = firstName && age
+
+if (isValid) {
+    console.log('Valid to the max!')
+}
