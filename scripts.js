2cad7bf3b58905d665770b0f6e781ab267f55c9f
larakoseff <larakoseff@gmail.com>
2023-08-21 16:10:36 +0200

    Add style element

--- scripts.js
+++ scripts.js
@@ -1,45 +1,68 @@
-(function(){"use strict";
-
-/** This is the signed in user */
-const user = 'Lara'
 
 const MAX_NUMBER = 15
 const MIN_NUMBER = -5
-const STEP_AMOUNT = 5;
+const STEP_AMOUNT = 1;
 
-const number = document.querySelector('[data-key="number"]')
-const subtract = document.querySelector('[data-key="subtract"]')
-const add = document.querySelector('[data-key="add"]')
+const elements = {
+number: document.querySelector('[data-key="number"]'),
+subtract: document.querySelector('[data-key="subtract"]'),
+add: document.querySelector('[data-key="add"]'), 
 
+}
+
+const updateColor = () => {
+
+    const value = parseInt(elements.number.value)
+    const singleStep = 250 / (MAX_NUMBER - MIN_NUMBER)
+
+    const distMax = MAX_NUMBER - value
+    const distMin = value - MIN_NUMBER
+
+    const red = distMax * singleStep 
+    const green = distMin *singleStep
+
+   elements.number.style.color = `rgb(${red}, ${green}, 250)` 
+
+}
 
 const subtractHandler = () => {
-    const newValue = parseInt(number.value) -STEP_AMOUNT
-    number.value = newValue
+    const newValue = parseInt(elements.number.value) -STEP_AMOUNT
+    elements.number.value = newValue
   
-    if (add.disabled === true) {
-        add.disabled = false
+    if (elements.add.disabled === true) {
+        elements.add.disabled = false
     }
 
     if (newValue <= MIN_NUMBER) {
-        subtract.disabled = true
+        elements.subtract.disabled = true
     }
+
+    updateColor ()
+
 }
 
 const addHandler = () => {
-    const newValue = parseInt(number.value) + STEP_AMOUNT
-    number.value = newValue
+    const newValue = parseInt(elements.number.value) + STEP_AMOUNT
+    elements.number.value = newValue
 
-    if (subtract.disabled === true) {
-        subtract.disabled = false
+    if (elements.subtract.disabled === true) {
+        elements.subtract.disabled = false
     }
 
 if (newValue >= MAX_NUMBER) {
-    add.disabled = true
+    elements.add.disabled = true
 }
+
+updateColor ()
+
 }
 
 
-subtract.addEventListener('click', subtractHandler )
-add.addEventListener('click', addHandler)
 
-})();
+elements.subtract.addEventListener('click', subtractHandler )
+elements.add.addEventListener('click', addHandler)
+
+updateColor ()
+
+
+
